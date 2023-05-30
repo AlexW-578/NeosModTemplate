4fc1ff6ac42f9b63d2cfdd5847acc5c39311d07b
AlexW-578 <66627294+AlexW-578@users.noreply.github.com>
2023-05-30 17:08:25 +0100

    Update ModNameGoesHere.cs
    Added Templates for Prefix, Postfix, and Transpiler

--- ModNameGoesHere/ModNameGoesHere.cs
+++ ModNameGoesHere/ModNameGoesHere.cs
@@ -9,19 +9,50 @@ namespace ModNameGoesHere
         public override string Author => "username";
         public override string Version => "0.0.1";
         public override string Link => "https://github.com/GithubUsername/RepoName/";
+	
+	[AutoRegisterConfigKey] private static readonly ModConfigurationKey<bool> Enabled = new ModConfigurationKey<bool>("Enabled", "Enable/Disable the Mod", () => true);
+	
         public override void OnEngineInit()
         {
+	    Config = GetConfiguration();
+            Config.Save(true);
             Harmony harmony = new Harmony("net.username.Template");
             harmony.PatchAll();
+        }
+	
+		
+        [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
+        class ModNameGoesHerePatch
+        {
+            static bool Prefix()
+            {
+                if (!Config.GetValue(Enabled)) { return true; }
+
+                return false;
+            }
         }
 		
         [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
         class ModNameGoesHerePatch
         {
-            public static bool Prefix()
+            static void Postfix()
             {
-                return false;//dont run rest of method
+                if (Config.GetValue(Enabled) )
+                {
+                    node.Up = __instance.Slot.ActiveUserRoot.Slot.Up;
+                }
             }
         }
+	
+        [HarmonyPatch(typeof(class you want to patch), "name of method you want to patch")]
+	public class Player_beeType_Patcher
+	{
+	    static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+	    {
+		var code = new List<CodeInstruction>(instructions);
+		// We'll need to modify code here.
+		return code;
+            }  
+	}
     }
 }
